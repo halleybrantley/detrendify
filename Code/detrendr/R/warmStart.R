03444b8b904fa2f3eb3a935ad533a6b6de068f82
Halley Brantley <halleybrantley@gmail.com>
2017-07-12 16:21:30 -0400

    warmStart added

---
+++ Code/detrendr/R/warmStart.R
@@ -0,0 +1,34 @@
+#' @title 
+#' Warm start for Spingarn algorithm 
+#' @description 
+#' Get approximation of initial values by aggregating data and fitting on 
+#' smaller dataset
+#' @param y original data to fit baseline
+#' @param k order of differencing matrix
+#' @param lambda regularization parameter
+#' @param tau quantile level
+#' @param reduction number of observations to aggregate
+#' @export
+warmStart <- function(y, k, lambda, tau, reduction){
+  x <- seq(1, length(y), 1)
+  df <- data.frame(y=y, x=x)
+  df$xBreaks <- cut(df$x, seq(1, length(x), reduction))
+  dfAgg <- ddply(df, .(xBreaks), summarize, 
+                 yMean = mean(y))
+  
+  y2 <- dfAgg$yMean
+  theta <- y2
+  n <- length(y2)
+  D <- get_Dk(n, k)
+  M <- Diagonal(n) + Matrix::crossprod(D)
+  cholM <- Matrix::chol(M)
+  eta <- matrix(D%*%theta)
+  multi_step <- spingarn_multi_step(theta, eta, y2, D, cholM,
+                                    lambda, tau, 1, 10000, k)
+  dfAgg$theta <- prox_f1(multi_step[[1]], y2, tau)
+  # plot(y2, type="l")
+  # lines(dfAgg$theta, col="red")
+  
+  dfAll <- merge(df, dfAgg)
+  return(dfAll$theta)
+}
