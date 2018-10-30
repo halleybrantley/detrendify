292e5dbb9d52984b8e4a79a2b1d5bf8b016ebdd4
Halley Brantley <halleybrantley@gmail.com>
2018-10-30 16:27:06 -0400

    fixed consensus functions to run on multiple quantiles and real data

--- Code/detrendr/R/consensus_ADMM.R
+++ Code/detrendr/R/consensus_ADMM.R
@@ -18,9 +18,6 @@
 #' k <- 3
 #' tau <- c(0.05)
 #' overlap <- 20
-#' w1 <- rep(0, overlap)
-#' w2 <- rep(0, overlap)
-#' z <- rep(0, overlap)
 #' rho <- 1
 #' result <- consensus_ADMM(y, tau, lambda, k, rho, overlap, 300)
 #' y_n <- length(y)
@@ -32,81 +29,99 @@
 #' lines(result$theta)
 #' plot(result$primal_norm)
 #' plot(result$dual_norm)
-consensus_ADMM <- function(y, tau, lambda, k, rho, overlap, max_iter){
+consensus_ADMM <- function(y, tau, lambda, k, rho, overlap, max_iter, eps = 0.01){
   y_n <- length(y)
-  window_size <- floor((y_n+overlap)/2)
+  nT <- length(tau)
+  if( (y_n + overlap) %% 2 != 0 ){
+    stop("length of y plus overlap must be even")
+  }
+
+  window_size <- (y_n+overlap)/2
 
   Ind1 <- (window_size-overlap+1):(window_size)
   Ind2 <- 1:overlap
-  
+
   y1 <- y[1:window_size]
   y2 <- y[(y_n-window_size+1):y_n]
-  
-  # Initial values
-  w1 <- rep(0, length(y1))
-  w2 <- rep(0, length(y2))
-  z1 <- rep(0, length(y1))
-  z2 <- rep(0, length(y2))
-  rho <- 1
 
-  
+  # Initial values
+  w1 <- numeric(length(y1)*length(tau))
+  w2 <- numeric(length(y2)*length(tau))
+  phiBar <- numeric(length(y1)*length(tau))
+
   # x-update
-  phi1 <- quad_update(y1, tau, lambda, k, overlap, w1, z1, 0, first = TRUE)
-  phi2 <- quad_update(y2, tau, lambda, k, overlap, w2, z2, 0, first = FALSE)
-  
+  phi1 <- quad_update(y1, tau, lambda, k, w1, phiBar, 0, first = TRUE)
+  phi2 <- quad_update(y2, tau, lambda, k, w2, phiBar, 0, first = FALSE)
+
   # Average update
-  phiBar <- numeric(length(y))
-  phiBar[1:window_size] <- phi1 
-  phiBar[(y_n-window_size+1):y_n] <- phi2
-  phiBar[(y_n-window_size+1):(y_n-window_size+overlap)] <- 
-    (phi1[Ind1] + phi2[Ind2])/2
-  
+  phiBar <- matrix(NA, nrow = y_n, ncol = length(tau))
+  phiBar[1:window_size,] <- phi1
+  phiBar[(y_n-window_size+1):y_n, ] <- phi2
+  phiBar[(y_n-window_size+1):(y_n-window_size+overlap),] <-
+    (phi1[Ind1,] + phi2[Ind2,])/2
+  phiBar1 <- as.numeric(phiBar[1:window_size,])
+  phiBar2 <- as.numeric(phiBar[(y_n-window_size+1):y_n, ])
+
   # Dual update
-  w1 <- w1 + rho*(phi1 - phiBar[1:window_size])
-  w2 <- w2 + rho*(phi2 - phiBar[(y_n-window_size+1):y_n])
+  w1 <- w1 + as.numeric(rho*(phi1 - phiBar[1:window_size,]))
+  w2 <- w2 + as.numeric(rho*(phi2 - phiBar[(y_n-window_size+1):y_n,]))
+
   phiBar_k <- phiBar
-  
+
   dual_norm <- double(max_iter)
   primal_norm <- double(max_iter)
+
   iter <- 1
 
   while(iter <= max_iter){
-    phi1 <- quad_update(y1, tau, lambda, k, overlap, w1, z, rho, first = TRUE)
-    phi2 <- quad_update(y2, tau, lambda, k, overlap, w2, z, rho, first = FALSE)
-    
+    phi1 <- quad_update(y1, tau, lambda, k, w1, phiBar1, rho, first = TRUE)
+    phi2 <- quad_update(y2, tau, lambda, k, w2, phiBar2, rho, first = FALSE)
+
     # Average update
-    phiBar <- numeric(length(y))
-    phiBar[1:window_size] <- phi1 
-    phiBar[(y_n-window_size+1):y_n] <- phi2
-    phiBar[(y_n-window_size+1):(y_n-window_size+overlap)] <- 
-      (phi1[Ind1] + phi2[Ind2])/2
-    
+    phiBar[1:window_size,] <- phi1
+    phiBar[(y_n-window_size+1):y_n, ] <- phi2
+    phiBar[(y_n-window_size+1):(y_n-window_size+overlap),] <-
+      (phi1[Ind1,] + phi2[Ind2,])/2
+    phiBar1 <- as.numeric(phiBar[1:window_size,])
+    phiBar2 <- as.numeric(phiBar[(y_n-window_size+1):y_n, ])
+
     # Dual update
-    w1 <- w1 + rho*(phi1 - phiBar[1:window_size])
-    w2 <- w2 + rho*(phi2 - phiBar[(y_n-window_size+1):y_n])
-    
+    w1 <- w1 + as.numeric(rho*(phi1 - phiBar[1:window_size,]))
+    w2 <- w2 + as.numeric(rho*(phi2 - phiBar[(y_n-window_size+1):y_n,]))
+
+    # plot(w1[1:window_size])
+    # plot(w1[(window_size+1):(2*window_size)])
+    # plot(w2[(window_size+1):(2*window_size)])
+    # plot(w2[1:window_size])
+
     # Convergence Metrics
-    dual_resid <- as.matrix(phiBar - phiBar_k)
+    dual_resid <- phiBar - phiBar_k
     dual_norm[iter] <- 2*rho*Matrix::norm(dual_resid, type = "F")
     phiBar_k <- phiBar
-    
-    primal_resid <- 
-      as.matrix(c(phi1 - phiBar[1:window_size],
-                  phi2 - phiBar[(y_n-window_size+1):y_n]))
-    
-    primal_norm[iter] <- Matrix::norm(primal_resid, type = "F") 
-    
-    if(dual_norm[iter] < 0.0005 & primal_norm[iter] < 0.05){
+
+    primal_resid <-
+      as.matrix(c(phi1 - phiBar[1:window_size,],
+                  phi2 - phiBar[(y_n-window_size+1):y_n,]))
+
+    primal_norm[iter] <- Matrix::norm(primal_resid, type = "F")
+
+    if (iter %% 20 == 0){
+      print(sprintf("Iteration: %d", iter))
+    }
+
+    if(dual_norm[iter] < eps*nT & primal_norm[iter] < eps*nT){
       primal_norm <- primal_norm[1:iter]
       dual_norm <- dual_norm[1:iter]
+      print(sprintf("Converged in %d iterations", iter))
       break
     }
     iter <- iter+1
   }
-  
+
   theta <- y - phiBar
-  return(list(theta = theta, phiBar = phiBar, phi1 = phi1, phi2 = phi2, 
-              primal_norm = primal_norm, dual_norm = dual_norm))
+  return(list(theta = theta, phiBar = phiBar, phi1 = phi1, phi2 = phi2,
+              primal_norm = primal_norm, dual_norm = dual_norm,
+              iter = iter))
 }
 
 
@@ -122,9 +137,12 @@ consensus_ADMM <- function(y, tau, lambda, k, rho, overlap, max_iter){
 #' @param overlap integer length of overlap between windows
 #' @param w value of dual variable
 #' @param rho step size for ADMM
-#' @param first boolean indicator of whether the first or second of the 
+#' @param first boolean indicator of whether the first or second of the
 #' overlapping windows
 quad_update <- function(y, tau, lambda, k, w, z, rho, first){
+  if(length(w) != length(z)) {
+    stop("w and z must be the same length")
+  }
   tau <- sort(tau)
   D <- as.matrix(get_Dk(length(y), k))
   n <- length(y)
@@ -134,69 +152,70 @@ quad_update <- function(y, tau, lambda, k, w, z, rho, first){
   missInd <- which(is.na(y))
   y[missInd] <- 0
   model <- list()
-  
+
   for (i in 1:nT){
     model$obj <- c(model$obj,
                    rep(tau[i], n), rep((1-tau[i]), n), rep(lambda, 2*m))
-    
+
     overlapInd <- (1+np*(i-1)):(2*n + np*(i-1))
-    
+    dualInd <- (1 + n*(i-1)):(n + n*(i-1))
+
     # Add in dual/consensus variables
-    model$obj[overlapInd] <- model$obj[overlapInd] + c((w-rho*z), -(w-rho*z))
+    model$obj[overlapInd] <- model$obj[overlapInd] +
+      c((w[dualInd]-rho*z[dualInd]), -(w[dualInd]-rho*z[dualInd]))
 
     model$obj[missInd] <- 0
     model$obj[missInd + n] <- 0
     model$rhs <- c(model$rhs, as.numeric(D%*%y))
   }
-  
+
   model$rhs <- c(model$rhs, rep(0, n*(nT-1)))
   model$Q <- matrix(0, length(model$obj), length(model$obj))
-  
-  
+
+
   # Constraint Matrix
   if (length(tau) == 1){
     model$A  <- cbind(D, -D, diag(m), -diag(m))
     diag(model$Q[overlapInd, overlapInd]) <- rho/2
-    
-    
+
   } else {
     model$A <- matrix(0, nrow =  m*nT + n*(nT-1), ncol= np*nT )
     for (i in 1:nT){
-      
+
       # Quadratic Term
       overlapInd <- (1+np*(i-1)):(2*n + np*(i-1))
       diag(model$Q[overlapInd, overlapInd]) <- rho/2
-      
+
       # D%*%theta = eta constraint
       model$A[(1+m*(i-1)):(m*i), (1+np*(i-1)):(np*i)] <-
         cbind(D, -D, diag(m), -diag(m))
-      
+
       # Non-crossing theta(tau) constrains
       if (i < nT){
         model$A[(m*nT+1+n*(i-1)):(m*nT+n*(i)), (1+(i-1)*np):(2*n + (i-1)*np)] <-
           cbind(diag(n), -diag(n))
-        
+
         model$A[(m*nT+1+n*(i-1)):(m*nT+n*(i)),
                 (1+i*np):(2*n + i*np)] <-
           cbind(-diag(n), diag(n))
       }
     }
   }
-  
+
   # Constraint Types
   model$sense <- c(rep('=', m*nT), rep(">", n*(nT-1)))
   model$modelsense <- "min"
-  
+
   # Type of variables (binary)
   params <- list(OutputFlag=0)
   result <- gurobi(model, params)
-  
+
   phi <- matrix(0, nrow=n, ncol = nT)
   for (i in 1:nT){
     phi[,i] <- result$x[(1+np*(i-1)):(n+np*(i-1))] -
       result$x[(n+1 + np*(i-1)):(2*n + np*(i-1))]
   }
-  
+
   return(phi)
 }
 
---
+++ Code/test_consensus.R
@@ -0,0 +1,72 @@
+library(devtools)
+load_all("detrendr")
+rm(list=ls())
+n <- 100
+x <- seq(1, n, 1)
+y <- sin(x*2*pi/n) + rnorm(n, 0, .4)
+lambda <- n
+k <- 3
+tau <- c(0.05, 0.5)
+overlap <- 20
+rho <- 1
+result <- consensus_ADMM(y, tau, lambda, k, rho, overlap, 300, 0.02)
+
+y_n <- length(y)
+window_size <- floor((y_n+overlap)/2)
+plot(result$phiBar[,1]~x, type="l")
+points(result$phi1[,1]~x[1:window_size])
+points(result$phi2[,1]~x[(y_n-window_size+1):y_n], col="blue")
+plot(y~x)
+lines(result$theta[,1], col="red")
+lines(result$theta[,2], col="blue")
+plot(result$primal_norm)
+plot(result$dual_norm)
+result$iter
+
+# Application
+dataDir <- "~/Desktop/EPA/SPod_Data/TestRange_Dec2017"
+datafiles <- dir(dataDir, pattern=".csv", full.names=TRUE)
+spod <- read.csv(datafiles[3], header=TRUE,  na.strings = "N/A")
+spod$time <- as.POSIXct(strptime(as.character(spod$TimeStamp), 
+                                 format= "%m/%d/%Y %H:%M:%S")) 
+
+node <- "h"
+pidCol <- paste(node, "SPOD.PID..V.", sep=".")
+spodNode <- spod[, c("time", pidCol)]
+names(spodNode)[2] <- c("pid")
+spodNode <- subset(spodNode, !is.na(pid))
+spodNode$pid <- as.numeric(scale(spodNode$pid, center = TRUE))
+
+plot(pid~time, spodNode[35000:36500, ], type="l")
+
+y <- spodNode[34801:36600, "pid"]
+x <- spodNode[34801:36600, "time"]
+lambda <- 10*length(y)
+k <- 3
+tau <- c(0.1, 0.5)
+overlap <- 300
+rho <- 1
+
+result <- consensus_ADMM(y, tau, lambda, k, rho, overlap, 200, 0.2)
+y_n <- length(y)
+window_size <- floor((y_n+overlap)/2)
+
+plot(result$phiBar[,2]~x, type="l")
+points(result$phi1[,2]~x[1:window_size])
+points(result$phi2[,2]~x[(y_n-window_size+1):y_n], col="blue")
+
+plot(y~x, type="l")
+lines(result$theta[,1]~x, col="red")
+lines(result$theta[,2]~x, col="blue")
+
+plot(result$primal_norm[20:40])
+plot(result$dual_norm[20:80])
+result$iter
+
+plot(y~x, type="l")
+lines(theta[,1]~x, col="red")
+lines(theta[,2]~x, col="blue")
+
+plot(phiBar[,2]~x, type="l")
+points(phi1[,2]~x[1:window_size])
+points(phi2[,2]~x[(y_n-window_size+1):y_n], col="blue")
