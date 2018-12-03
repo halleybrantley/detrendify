869927b18c876e240fc2a59ee9e04c319ab345ae
Halley - henry2 <hlbrantl@ncsu.edu>
2018-12-03 12:45:32 -0500

    batch script updates

--- Code/Sim_detrendr.R
+++ Code/Sim_detrendr.R
@@ -2,12 +2,12 @@ library(devtools)
 load_all("detrendr")
 rm(list=ls())
 tau <- c(0.01, 0.05, 0.25, 0.5, .75, 0.95, 0.99)
-nSim <- 100
-n <- 300
+#nSim <- 100
+n <- 500
 simDesigns <- c("gaus", "mixednorm", "shapebeta", "peaks")
-
+i = 0
 for(simDesign in simDesigns){                
-  for (i in 1:nSim){
+#  for (i in 1:nSim){
     load(sprintf("../SimData/%s_n_%i_sim%03.0f.RData", simDesign, n, i))
     lam_SIC <- lambda_SIC(df$y, tau, 3, 
                           lambdaSeq = seq(n/20,n,n/40), 
@@ -21,5 +21,5 @@ for(simDesign in simDesigns){
     save(trend, lam_valid,
          file = sprintf("../SimResults/detrend_valid/%s_n_%i_sim%03.0f.RData", 
                         simDesign, n, i))
-  }
+#  }
 }
