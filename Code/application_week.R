b1a4113f96c298451a762d36620a8e0a791b2fa5
Halley Brantley <halleybrantley@gmail.com>
2019-03-20 16:47:02 -0400

    added code for week of applied data, fixed bug in get_trend_windows

--- Code/application_week.R
+++ Code/application_week.R
@@ -25,14 +25,11 @@ spodPIDs$time <- spod$time
 qsreg_trends <- data.frame(time = spodPIDs$time, c_0.1 = NA, 
                         c_0.15 = NA,  e_0.1 = NA, e_0.15 = NA)
 spod_trends <- data.frame(time = spod$time)
-window_size <- 10000
+window_size <- 5000
 overlap <- 1000
-max_iter <- 5
+max_iter <- 30
 tau <- c(0.1, 0.15)
-k <- 3
 
-
-tau <- c(0.1, 0.15)
 t1 <- Sys.time()
 for (j in 1:12){
   ind_start <- (j-1)*7200 + 1
@@ -42,33 +39,34 @@ for (j in 1:12){
   trends <- data.frame(time = spodPIDs$time[ind_start:ind_end])
   for (node in nodes){
     trend <- matrix(NA, n, length(tau))
-    for (k in 1:length(tau)){
+    for (i in 1:length(tau)){
       fit_qsreg <- qsreg(x, spodPIDs[ind_start:ind_end,node], 
                          maxit.cv = 50, 
-                       alpha=tau[k], hmin = -6, hmax = NA)
-      trend[,k] <- predict(fit_qsreg)   
+                       alpha=tau[i], hmin = -6, hmax = NA)
+      trend[,i] <- predict(fit_qsreg)   
     }
     trends <- cbind(trends, as.data.frame(trend))
     names(trends)[(ncol(trends)-(length(tau)-1)):ncol(trends)] <-
       paste(node, tau, sep = "_")
   }
   qsreg_trends[ind_start:ind_end, ] <- trends
-  
-  for (node in nodes){
-    result <- get_windows_BIC(spodPIDs[,node], tau, k, window_size, overlap,
-                              lambdaSeq = exp(seq(12,19,1)),
-                              df_tol = 1e-9,
-                              gamma = 1,
-                              plot_lambda = TRUE,
-                              solver = NULL,
-                              criteria = "eBIC", 
-                              max_iter = max_iter, 
-                              rho = 1, 
-                              update = 2)
-    spod_trends <- cbind(spod_trends, as.data.frame(result$trend))
-    names(spod_trends)[(ncol(spod_trends)-length(tau)+1):ncol(spod_trends)] <-
-      paste(node, tau, sep = "_")
-  }
 }
 
-save(spod_trends, qsreg_trends, spodPIDs, file = )
+for (node in nodes){
+  result <- get_windows_BIC(y=spodPIDs[,node], tau, k=3, window_size, overlap,
+                            lambdaSeq = seq(12,19,1)),
+                            df_tol = 1e-9,
+                            gamma = 1,
+                            plot_lambda = FALSE,
+                            solver = NULL,
+                            criteria = "eBIC", 
+                            max_iter = max_iter, 
+                            rho = 1, 
+                            update = 1)
+  spod_trends <- cbind(spod_trends, as.data.frame(result$trend))
+  names(spod_trends)[(ncol(spod_trends)-length(tau)+1):ncol(spod_trends)] <-
+    paste(node, tau, sep = "_")
+}
+
+save(spod_trends, qsreg_trends, spodPIDs, 
+     file = sprintf("../Data/SPod/SPod_week/trends_2017-03-0%d.RData",i))
