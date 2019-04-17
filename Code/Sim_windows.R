c253017e3f6b3cc34bec918820b86102f1c09720
Halley Brantley <halleybrantley@gmail.com>
2019-04-17 11:56:55 -0400

    more updates

--- Code/Sim_windows.R
+++ Code/Sim_windows.R
@@ -6,7 +6,7 @@ tau <- c(0.01, 0.05, 0.1)
 simDesigns <- c("gaus", "mixednorm", "shapebeta", "peaks")
 simDesigns <- "peaks" 
 
-i = 1
+for (i in c(24, 19, 58, 91, 78, 72, 71, 31, 76, 86)){ 
 for (n in c(500,1000,2000,4000)){
   lambdaSeq = n^seq(0,1.4,length.out=15) 
   overlap <- n/10
@@ -16,12 +16,16 @@ for (n in c(500,1000,2000,4000)){
     trend_BIC <- get_windows_BIC(df$y, tau, k=3,
                           lambdaSeq = lambdaSeq,
                           window_size = window_size, 
-                          overlap = overlap,
+                          overlap = overlap, 
+                          rho = 1,
+                          max_iter = 20,
                           plot_lambda = FALSE,
                           criteria = "eBIC")
+    
     trend <- trend_BIC$trend
     save(trend, trend_BIC,
          file = sprintf("../SimResults/windows/%s_n_%i_sim%03.0f.RData",
                         simDesign, n, i))
   }
+}
 }
